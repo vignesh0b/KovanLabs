f77957c751c91932c4c144ffc4118de18710d56e
vignesh0b <bvignes01@gmail.com>
2026-02-05 17:45:57 +0530

    Added java day9 exercise

---
+++ src/com/kovanlabs/intern/java/oop/composition/Amd08Ram.java
@@ -0,0 +1,8 @@
+package com.kovanlabs.intern.java.oop.composition;
+
+public class Amd08Ram implements Ram{
+    @Override
+    public void spec() {
+        System.out.println("This is Amd 8gb ram");
+    }
+}
