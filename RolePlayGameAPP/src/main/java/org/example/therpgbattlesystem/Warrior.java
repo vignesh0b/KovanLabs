4512c928209f547642c3c5012c897f3209fb1bf6
vignesh0b <bvignes01@gmail.com>
2026-02-24 13:32:13 +0530

    project structural changes

---
+++ RolePlayGameAPP/src/main/java/org/example/therpgbattlesystem/Warrior.java
@@ -0,0 +1,14 @@
+package org.example.therpgbattlesystem;
+
+public class Warrior extends Character{
+    public Warrior(String name) {
+        super(name, 150, 15);
+    }
+
+    @Override
+    public void attack(Character opponent) {
+        int damage = strength + random.nextInt(5);
+        System.out.println(name + " slashes for "+damage +" damage");
+        opponent.takeDamage(damage);
+    }
+}
